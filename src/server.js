3f52c8f8fcc222576809097f47ff3ef6444cdf5c
Jan Majek <jan.majek@ft.com>
2018-10-23 08:34:16 +0100

    Refactor app from server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
-import express from 'express';
 import debug from './utils/debug';
+import app from './app';
 
 const port = process.env.PORT || 3000;
 process.env.APP_NAME = process.env.APP_NAME || 'simple-cc-api';
-
-const app = express();
 
 app.listen(port, () => {
   debug('server')('Listening on port', port);
