60edfa26070c26e2c6e0ff9f83a185f545fa83cc
Jan Majek <jan.majek@ft.com>
2018-10-23 09:34:15 +0100

    Add create card API endpoint

--- src/routes/cards.js
+++ src/routes/cards.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import Card from '../core/card';
 import CardStore from '../core/cardStore';
+import { createCardValidator } from '../middleware/validators';
 
 const router = express.Router();
 const cardStore = new CardStore();
@@ -7,5 +9,16 @@ const cardStore = new CardStore();
 router.get('/', (req, res) => {
   res.send(cardStore.getAll());
 });
+
+router.post(
+  '/',
+  createCardValidator(),
+  (req, res) => {
+    const { name, number, limit } = req.body;
+    const limitNumber = Number.parseFloat(limit.substring(1));
+    const card = new Card(name, Number.parseInt(number, 10), limitNumber);
+    cardStore.add(card);
+    res.sendStatus(201);
+  });
 
 export default router;
--- tests/addCardApi.test.js
+++ tests/addCardApi.test.js
@@ -7,4 +7,59 @@ describe('Add credit card API', () => {
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual([]);
   });
+
+  it('should validate input when adding card', async () => {
+    const response = await request(app).post('/cards').send({
+      name: '',
+      number: '',
+      limit: '',
+    });
+    expect(response.statusCode).toBe(400);
+    expect(response.body.error).toBe('Validation error: instance.name does not meet minimum length of 1');
+  });
+
+  it('should add valid cards', async () => {
+    const [response, response2] = await Promise.all([
+      request(app).post('/cards').send({
+        name: 'card1',
+        number: '79927398713',
+        limit: '£1000.00',
+      }),
+      request(app).post('/cards').send({
+        name: 'card2',
+        number: '4539058051444578',
+        limit: '£100.00',
+      }),
+    ]);
+    expect(response.statusCode).toBe(201);
+    expect(response2.statusCode).toBe(201);
+  });
+
+  it('should not add duplicate card', async () => {
+    const response = await request(app).post('/cards').send({
+      name: 'card1',
+      number: '79927398713',
+      limit: '£1000.00',
+    });
+    expect(response.statusCode).toBe(409);
+  });
+
+  it('should return added card', async () => {
+    const response = await request(app).get('/cards');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual([
+      {
+        name: 'card1',
+        number: 79927398713,
+        limit: 1000,
+        balance: 0,
+      },
+      {
+        name: 'card2',
+        number: 4539058051444578,
+        limit: 100,
+        balance: 0,
+      },
+    ]);
+  });
 });
