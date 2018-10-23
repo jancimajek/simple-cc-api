e8f1c8af09b40578be748c6de292b5544cfb2e22
Jan Majek <jan.majek@ft.com>
2018-10-23 09:11:30 +0100

    Use error handler middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cards from './routes/cards';
+import errorHandler from './middleware/errorHandler';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/cards', cards);
+app.use(errorHandler);
 
 export default app;
