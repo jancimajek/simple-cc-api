b91fc72b3706e203054cb396a1120bd556047e46
Jan Majek <jan.majek@ft.com>
2018-10-23 07:41:35 +0100

    Add card to store

--- src/core/__tests__/cardStore.test.js
+++ src/core/__tests__/cardStore.test.js
@@ -1,8 +1,27 @@
 import CardStore from '../cardStore';
+import Card from '../card';
 
 describe('Card Store', () => {
   it('should be empty at beginning', () => {
     const cardStore = new CardStore();
     expect(cardStore.store).toEqual({});
+  });
+
+  it('should add a new card if it does not exist', () => {
+    const cardStore = new CardStore();
+    const card = new Card('name', 79927398713, 1000);
+    cardStore.add(card);
+
+    expect(cardStore.store.name).toBe(card);
+  });
+
+  it('should throw error when adding card that already exists', () => {
+    const cardStore = new CardStore();
+    const card = new Card('name', 79927398713, 1000);
+    const card2 = new Card('name', 79927398713, 1000);
+    cardStore.add(card);
+
+    expect(() => cardStore.add(card)).toThrow('Card already exists: name');
+    expect(() => cardStore.add(card2)).toThrow('Card already exists: name');
   });
 });
--- src/core/cardStore.js
+++ src/core/cardStore.js
@@ -2,4 +2,13 @@ export default class CardStore {
   constructor() {
     this.store = {};
   }
+
+  add(card) {
+    const { name } = card;
+    if (this.store[name]) {
+      throw new Error(`Card already exists: ${name}`);
+    }
+
+    this.store[name] = card;
+  }
 }
