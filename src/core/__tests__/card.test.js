7b48541cdbe1e96be49702c6bed5bd5ddb76b65e
Jan Majek <jan.majek@ft.com>
2018-10-23 07:06:15 +0100

    Charge card

--- src/core/__tests__/card.test.js
+++ src/core/__tests__/card.test.js
@@ -14,3 +14,18 @@ describe('Create card', () => {
     expect(() => new Card('name', 79927398710, 1000)).toThrow('Invalid card number: 79927398710');
   });
 });
+
+describe('Charge card', () => {
+  it('should return correct balance', () => {
+    const card = new Card('name', 79927398713, 1000);
+    expect(card.charge(10)).toBe(10);
+    expect(card.charge(10.01)).toBe(20.01);
+    expect(card.charge(99.99)).toBe(120.00);
+    expect(card.charge(880)).toBe(1000);
+  });
+
+  it('should throw error when charge goes over the limit', () => {
+    const card = new Card('name', 79927398713, 1000);
+    expect(() => card.charge(1000.01)).toThrow('Credit limit exceeded for card number: 79927398713');
+  });
+});
--- src/core/card.js
+++ src/core/card.js
@@ -1,3 +1,4 @@
+import debug from '../utils/debug';
 import { checkSumLuhn10 } from '../utils/luhn10';
 
 export default class Card {
@@ -10,5 +11,15 @@ export default class Card {
     this.number = number;
     this.limit = limit;
     this.balance = balance;
+  }
+
+  charge(ammount) {
+    debug('card')('Charge', ammount);
+    if (this.balance + ammount > this.limit) {
+      throw new Error(`Credit limit exceeded for card number: ${this.number}`);
+    }
+
+    this.balance = Math.round((this.balance + ammount) * 100) / 100;
+    return this.balance;
   }
 }
